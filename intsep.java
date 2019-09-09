407e4044bbba155297ee1b95b2aa1b41d89baeaa
BreadJuice1 <43072844+BreadJuice1@users.noreply.github.com>
2019-09-09 12:14:28 -0700

    Update intsep.java

--- intsep.java
+++ intsep.java
@@ -8,12 +8,11 @@ class intsep {
     // get number
     System.out.print("input any amount of any alphanumeric character: ");
     String number = in.next();
+    in.close();
 
     // seperate digits and print
     for (int i = 1; i <= number.length(); i++) {
         System.out.print(number.substring(i-1,i) +"   ");
-        }
-
-        in.close();
+        } // for
     } // main
 } // class
