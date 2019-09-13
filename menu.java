22fe82aec9428254c73d65377a9bea0f146ea1e9
BreadJuice1 <43072844+BreadJuice1@users.noreply.github.com>
2019-09-13 13:19:29 -0700

    Add files via upload

--- menu.java
+++ menu.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 class menu {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        File folder = new File(in.next());
+        File folder = new File(System.getProperty("user.dir"));
 File[] listOfFiles = folder.listFiles();
 for (int i = 0; i < listOfFiles.length; i++) {
   if (listOfFiles[i].isFile()) {
