d9ba4c6e608758607249d419593e2de2bbb581e7
BreadJuice1 <43072844+BreadJuice1@users.noreply.github.com>
2019-09-17 12:48:30 -0700

    Add files via upload

--- lab3.java
+++ lab3.java
@@ -1,4 +1,6 @@
-import java.util.InputMismatchException; import java.util.Scanner; import java.lang.Math;
+import java.util.InputMismatchException; import java.util.Scanner;
+
+import java.lang.Math;
 
 class lab3 {
     public static void main(String[] args) {
@@ -9,8 +11,8 @@ class lab3 {
 
     do {
         // check program
-        System.out.print("input program:");
             try {
+        System.out.print("input program:");
         programNum = in.nextInt();
         if (programNum != 0 && programNum != 1 && programNum != 2) {
             System.out.println("number | program");
@@ -27,21 +29,22 @@ class lab3 {
             // get a, b, and c
             Scanner nums = new Scanner(System.in);
             System.out.print("input a:");
-                Double a = nums.nextDouble();
+            Double a = nums.nextDouble();
             System.out.print("input b:");
-                Double b = nums.nextDouble();
+            Double b = nums.nextDouble();
             System.out.print("input c:");
-                Double c = nums.nextDouble();
+            Double c = nums.nextDouble();
             nums.close();
 
             // math
-                double x1 = (-b + Math.sqrt(Math.pow(b, 2) - (4 * a * c))) / (2 * a);
-                double x2 = (-b - Math.sqrt(Math.pow(b, 2) - (4 * a * c))) / (2 * a);
+            double x1 = (-b + Math.sqrt(Math.pow(b, 2) - (4 * a * c))) / (2 * a);
+            double x2 = (-b - Math.sqrt(Math.pow(b, 2) - (4 * a * c))) / (2 * a);
     
             if (Double.isNaN(x1) || Double.isNaN(x2))
             {
                 System.out.println("x is an imaginary number");
             } else System.out.println("The values are: " + x1 + ", " + x2);
+            break;
         } // program 1
     
         if (programNum == 2) {
@@ -49,9 +52,9 @@ class lab3 {
             // get a and b
             Scanner nums = new Scanner(System.in);
             System.out.print("input a:");
-                Double a = nums.nextDouble();
+            Double a = nums.nextDouble();
             System.out.print("input b:");
-                Double b = nums.nextDouble();
+            Double b = nums.nextDouble();
             nums.close();
 
             // math
@@ -60,17 +63,18 @@ class lab3 {
 
             // return answers
             System.out.print("c = " +c);
+            break;
 
         } // program 2
 
         if (programNum == 0) {
              System.exit(0);
         } // program 3
-    } catch(InputMismatchException e) {
+    } catch(Exception e) {
         System.out.print("please input a number.");
         System.exit(0);
             } 
-    } while (programNum == -1);
-     in.close();
+    } while (programNum != 0);
+    in.close();
     } // main
 } // class
