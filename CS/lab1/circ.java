b676904a7496c98de4d3d1228eecf57f3f20333b
BreadJuice1 <43072844+BreadJuice1@users.noreply.github.com>
2019-10-24 22:38:53 -0700

    Update circ.java

--- CS/lab1/circ.java
+++ CS/lab1/circ.java
@@ -2,6 +2,14 @@ import java.util.*;
 
 class circ {
     public static void main(String[] args) {
-        
+
+        Scanner in = new Scanner(System.in);
+        Double circ;
+        System.out.print("Input the radius:");
+        Double radius = in.nextDouble();
+
+        circ = ((2) * (3.14159) * (radius));
+        System.out.print("the circumference is" + circ);
+
     }
 }
