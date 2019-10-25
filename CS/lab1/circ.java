7a7829e13d5ed24dafd708bead405a754d3b1e11
BreadJuice1 <43072844+BreadJuice1@users.noreply.github.com>
2019-10-24 22:47:47 -0700

    closed in

--- CS/lab1/circ.java
+++ CS/lab1/circ.java
@@ -6,6 +6,7 @@ class circ {
         Scanner in = new Scanner(System.in);
         Double circ;
         System.out.print("Input the radius:");
+        in.close();
         Double radius = in.nextDouble();
 
         circ = ((2) * (3.14159) * (radius));
