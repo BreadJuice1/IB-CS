f76e1ba09ab8dcd67053dee4b4ee251ef93f06d4
BreadJuice1 <43072844+BreadJuice1@users.noreply.github.com>
2019-10-29 12:21:01 -0700

    added pdfs

---
+++ IB-CS-master/IB-CS-master/CS/lab2/oddeven.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class oddeven {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        // get number
+        System.out.print("input number: ");
+        Double number = in.nextDouble();
+
+        // math
+        Boolean check = number % 2 == 0;
+        if (check == true) {
+            System.out.println(number + " is an even number");
+        } else {
+            System.out.println(number + " is an odd number");
+        }
+        in.close();
+    } // main
+} // class
---
+++ IB-CS-master/IB-CS-master/lab4.java
@@ -0,0 +1,111 @@
+ import java.util.Scanner;
+
+ class lab4 {
+     public static void sigma() {
+        Scanner in = new Scanner(System.in);
+      
+        System.out.print("input number:");
+            int num = in.nextInt();
+        in.close();
+
+        int sum = 0;
+        for (int i = 1; i <= num; i++) {
+            sum = sum + i;
+            if (i < num) {
+            System.out.print(i +"+");
+                } else {
+                    System.out.print(i);    
+                }
+            } // for
+      
+            System.out.print("=" +sum);
+    } // sigma
+
+    public static void grades() {
+        Scanner grades = new Scanner(System.in);
+      
+        int grade = 0;
+        int average = 0;
+        int counter = 0;
+        String averageGrade = "bruh";
+        int amountA = 0;
+        int amountB = 0;
+        int amountC = 0;
+        int amountD = 0;
+        int amountF = 0;
+    
+        do {
+            System.out.print("input grade:");
+          
+            grade = grades.nextInt();
+            
+            if (grade != -1) {
+            average = (average * counter + grade) / (counter + 1);
+            
+            if (grade >= 90) {
+                amountA++;
+            } else if (grade >= 80 && grade < 90) {
+                amountB++;
+            } else if (grade >= 70 && grade < 80) {
+                amountC++;
+            } else if (grade >= 60 && grade < 70) {
+                amountD++;
+            } else {
+                amountF++;
+            }
+        }
+            counter++;
+
+        } while (grade != -1); 
+        
+        if (average >= 90) { 
+        averageGrade = "A";
+        } else if (average >= 80 && average < 90) {
+        averageGrade = "B";
+        } else if (average >= 70 && average < 80) {
+        averageGrade = "C";
+        } else if (average >= 60 && average < 70) {
+        averageGrade = "D";
+        } else {
+        averageGrade = "F";
+        }
+
+        System.out.println("the average is " +averageGrade);
+        System.out.println(amountA +" studnets got an A");
+        System.out.println(amountB +" students got a B");
+        System.out.println(amountC +" students got a C");
+        System.out.println(amountD +" students got a D");
+        System.out.println(amountF +" students got a F");
+        
+        grades.close();
+    } // grades
+
+    public static void reciprocal() {
+    
+        Scanner in = new Scanner(System.in);
+        
+        double num = 0;
+        double sum = 0;
+        double recip = 0;
+
+        for (int i = 0; i<=10; i++) {
+        
+        }
+
+    }
+    
+    public static void main(String[] args) {
+        Scanner program = new Scanner(System.in);
+        System.out.println("which problem do you want to run?");
+
+        int prognum = program.nextInt();
+        
+        if (prognum == 0) {
+            System.exit(0);
+        } else if (prognum == 1) {
+                sigma();
+            } else if (prognum == 2) {
+                    grades();
+                }
+    } // main
+ } // class
