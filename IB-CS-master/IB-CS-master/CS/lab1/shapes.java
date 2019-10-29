51d9783bbc73be0394a17299c988af7144dd6663
BreadJuice1 <43072844+BreadJuice1@users.noreply.github.com>
2019-10-29 13:02:48 -0700

    cleaned stuff up

--- IB-CS-master/IB-CS-master/CS/lab1/shapes.java
+++
@@ -1,9 +0,0 @@
-
-class shapes {
-    public static void main(String[] args) {
-        System.out.print("\n * * * * * \n *       * \n *       * \n * * * * * \n"); // box 
-        System.out.print("\n   * \n  * * \n *   * \n  * * \n   * \n"); // diamond
-        System.out.print("\n   * * \n  *   * \n   * * \n"); // oval
-        System.out.print("\n    * \n   * * \n  *   * \n *     * \n* * * * * \n    * \n    * \n    * \n    * \n"); // arrow
-    }
-}
