d6f30c5e6a2a00a48ff194d417d5c89078ab89f1
BreadJuice1 <43072844+BreadJuice1@users.noreply.github.com>
2019-10-31 12:59:45 -0700

    broke more stuff

--- CS/lab5/lab5.java
+++ CS/lab5/lab5.java
@@ -17,7 +17,7 @@ class charread {
 }// charread
 
 class lab5 {
-    public static void one(String args[]) throws IOException {
+    public static void main(String args[]) throws IOException {
         int start = 0;
         int end = 0;
         System.out.print("input a start letter:");
@@ -26,16 +26,8 @@ class lab5 {
         end = System.in.read();
 
         for (int i = start; i <= end; i++) {
-            System.out.println(i + (char) i);
+            char letter = (char) i;
+            System.out.println(i + letter);
         }
-    } // problem1
-
-    public static void main(String[] args) {
-        Scanner prog = new Scanner(System.in);
-        System.out.println("what program do you want to run?");
-        int prognum = prog.nextInt();
-        if (prognum == 1) { one(args); }
-        else if (prognum == 2) { }
-
-    }
+    } // problem2
 } // class
