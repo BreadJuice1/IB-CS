4efe1a7c046bee9c3f3208f8b46499b581dd5036
BreadJuice1 <43072844+BreadJuice1@users.noreply.github.com>
2019-10-31 12:28:29 -0700

    test

--- CS/lab5/lab5.java
+++ CS/lab5/lab5.java
@@ -23,6 +23,6 @@ class lab5 {
     } // problem1
 
     public static void main(String[] args) {
-        System.out.print("which problem do you want to run?")
+        System.out.print("which problem do you want to run?");
     }
 } // class
