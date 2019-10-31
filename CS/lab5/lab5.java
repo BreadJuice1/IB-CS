52f5039e02ba3e02c6ad9fff39d6ceae9f031a43
BreadJuice1 <43072844+BreadJuice1@users.noreply.github.com>
2019-10-31 12:44:28 -0700

    `

--- CS/lab5/lab5.java
+++ CS/lab5/lab5.java
@@ -1,28 +1,36 @@
 import java.io.*;
+import java.util.*;
 
 class charread {
     public static void main(String args[]) throws IOException {
         int charval = 0;
         char mychar;
-        
+
         System.out.println("Please enter a character");
-        
+
         charval = System.in.read();
-        
+
         mychar = (char) charval;
-        
+
         System.out.println("Letter value read was " + charval + " character is " + mychar);
     } // main
 }// charread
 
 class lab5 {
-    public static void problem1() {
+    public static void one(String args[]) throws IOException {
+        int start = 0;
+        int end = 0;
+        System.out.print("input a start letter:");
+        start = System.in.read();
+        System.out.print("input an end letter:");
+        end = System.in.read();
 
-
-
+        for (int i = start; i <= end; i++) {
+            System.out.println(i + (char) i);
+        }
     } // problem1
 
     public static void main(String[] args) {
-        System.out.print("which problem do you want to run?");
+
     }
 } // class
