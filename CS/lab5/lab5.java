acc82c64929f60f30f26488f98b2e97a7bf33922
BreadJuice1 <43072844+BreadJuice1@users.noreply.github.com>
2019-10-31 13:13:51 -0700

    CS/lab5/lab5:

--- CS/lab5/lab5.java
+++ CS/lab5/lab5.java
@@ -23,7 +23,7 @@ class lab5 {
         System.out.print("input a start letter:");
         start = System.in.read();
         System.out.print("input an end letter:");
-        end = System.in.read();
+        end = System.in.read();h
 
         for (int i = start; i <= end; i++) {
             char letter = (char) i;
