376563a95b84abbd9ab1afb7b48d7634dde8dc1d
BreadJuice1 <43072844+BreadJuice1@users.noreply.github.com>
2019-11-05 13:16:05 -0800

    did stuff

--- CS/lab5/lab5.java
+++ CS/lab5/lab5.java
@@ -23,10 +23,8 @@ class two {
 
         System.out.print("input a start letter and an end letter:");
         start = System.in.read();
-        startchar = (char) start;
 
         end = System.in.read();
-        endchar = (char) end;
 
         for (int i = start; i <= end; i++) {
             System.out.print(i + " ");
@@ -37,12 +35,14 @@ class two {
 } // class two
 
 class three {
-    public static void main(String[] args) {
-        int integer[] = new int[6];
+    public static void main(String[] args) throws IOException {
+        int integer[] = new int[7];
 
         System.out.print("input an up to six digit number:");
-        for (i = 0; i <= 6; i++) {
+        for (int i = 0; i <= 6; i++) {
             integer[i] = System.in.read();
         }
+
+        System.out.print(integer[0] - (int) '0');
     }
 }
