299ccfae2b41ea7eb3ebfb04cd226f4cc322bab6
BreadJuice1 <43072844+BreadJuice1@users.noreply.github.com>
2019-11-05 12:57:57 -0800

    started problem 3

--- CS/lab5/lab5.java
+++ CS/lab5/lab5.java
@@ -29,9 +29,20 @@ class two {
         endchar = (char) end;
 
         for (int i = start; i <= end; i++) {
-            System.out.print(i +" ");
-            System.out.print((char) i +"\n");
+            System.out.print(i + " ");
+            System.out.print((char) i + "\n");
         }
 
     } // main
 } // class two
+
+class three {
+    public static void main(String[] args) {
+        int integer[] = new int[6];
+
+        System.out.print("input an up to six digit number:");
+        for (i = 0; i <= 6; i++) {
+            integer[i] = System.in.read();
+        }
+    }
+}
