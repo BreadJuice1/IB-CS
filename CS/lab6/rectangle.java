c617ca7077d3ca503531751e8c163b0df13db84e
BreadJuice1 <43072844+BreadJuice1@users.noreply.github.com>
2019-12-03 13:36:30 -0800

    Update rectangle.java

--- CS/lab6/rectangle.java
+++ CS/lab6/rectangle.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 class rectangle {
     public static void main(String[] args) {
         int height;
-        int width;
+        int width; 
         Scanner in = new Scanner(System.in);
 
         System.out.print("input the height and width seperated by a space:");
@@ -11,8 +11,6 @@ class rectangle {
         width = in.nextInt();
 
         String box[][] = new String[width][height];
-
-        
 
     }
 }
