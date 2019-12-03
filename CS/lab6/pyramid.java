1df34094a7af82c7a966753c8c0b2bdc14849650
BreadJuice1 <43072844+BreadJuice1@users.noreply.github.com>
2019-12-03 12:50:52 -0800

    summary

--- CS/lab6/pyramid.java
+++ CS/lab6/pyramid.java
@@ -3,14 +3,15 @@ import java.io.IOException;
 class pyramid {
     public static void main(String[] args) throws IOException {
         int height = 0;
-        int counter = 1;
+        int i = 1;
 
         System.out.println("what number do you want to start at?");
-        height = (char) System.in.read();
+        height = System.in.read() - 48;
 
-        for (int i = height; i >= 0; i--) {
-            System.out.print(i);
+        for (int j = height; j >= 0; j--) {
+            for (i = 1; i <= height; i++) {
+                System.out.print(j);
             }
-
+        }   System.out.println();
     }
 }
