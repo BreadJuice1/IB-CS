36750c89ab30a4d36708353a62a7c428ee489224
BreadJuice1 <43072844+BreadJuice1@users.noreply.github.com>
2019-12-03 13:34:06 -0800

    Create rectangle.java

---
+++ CS/lab6/rectangle.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class rectangle {
+    public static void main(String[] args) {
+        int height;
+        int width;
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("input the height and width seperated by a space:");
+        height = in.nextInt();
+        width = in.nextInt();
+
+        String box[][] = new String[width][height];
+
+        
+
+    }
+}
