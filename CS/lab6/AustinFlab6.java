b65722eebcd9ce39a25813d61c0bbc3e3603b598
BreadJuice1 <43072844+BreadJuice1@users.noreply.github.com>
2019-12-05 13:09:19 -0800

    Update AustinFlab6.java

--- CS/lab6/AustinFlab6.java
+++ CS/lab6/AustinFlab6.java
@@ -1,6 +1,19 @@
+import java.util.Scanner;
 
-class lab6 {
+class pyramid {
     public static void main(String[] args) {
-        
+        int height = 0;
+        int i = 1;
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("what number do you want to start at?");
+        height = in.nextInt();
+
+        for (int j = height - 1; j > 0; j--) {
+            for (i = j; i <= height; i++) {
+                System.out.print(j + "   ");
+            }
+            System.out.println();
+        }
     }
 }
