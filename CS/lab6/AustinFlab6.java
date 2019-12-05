fe1493a9dc7cbb8c18ac406102467f0c0e64f66b
BreadJuice1 <43072844+BreadJuice1@users.noreply.github.com>
2019-12-05 13:10:42 -0800

    Update AustinFlab6.java

--- CS/lab6/AustinFlab6.java
+++ CS/lab6/AustinFlab6.java
@@ -1,3 +1,5 @@
+import java.io.*;
+import java.util.Random;
 import java.util.Scanner;
 
 class pyramid {
@@ -17,3 +19,23 @@ class pyramid {
         }
     }
 }
+
+class game {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        Scanner in = new Scanner(System.in);
+        int number = rand.nextInt(1001);
+        int guess = 0;
+
+        do {
+            System.out.print("guess a number between 0 and 1000:");
+            guess = in.nextInt();
+            if (guess > number) {
+                System.out.println("too high");
+            } else if (guess < number) {
+                System.out.println("too low");
+            }
+        } while (guess != number);
+        System.out.println("you found the number!");
+    } // main
+} // class
