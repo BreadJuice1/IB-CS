3de423beb11b8c5a7e1751f44cdc72974ea32ac4
BreadJuice1 <43072844+BreadJuice1@users.noreply.github.com>
2019-12-05 13:35:58 -0800

    completed lab6 problem 3

--- CS/lab6/AustinFlab6.java
+++ CS/lab6/AustinFlab6.java
@@ -39,3 +39,51 @@ class game {
         System.out.println("you found the number!");
     } // main
 } // class
+
+class rectangle {
+    public static void main(String[] args) {
+        int height;
+        int width;
+        String box = "";
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("input the height and width seperated by a space:");
+        height = in.nextInt();
+        width = in.nextInt();
+
+        int w = width;
+        int h = height;
+        while (w > 0) {
+            box += "*";
+            w--;
+        } // top
+        System.out.println(box);
+
+        box = "*";
+
+        while (h > 2) {
+
+            w = width;
+
+            while (w > 2) {
+                box += " ";
+                w--;
+            } // middle
+            box += "*";
+            System.out.println(box);
+            box = "*";
+            h--;
+        } // height
+
+        box = "";
+        w = width;
+
+        while (w > 0) {
+            box += "*";
+            w--;
+        } // bottom
+        System.out.println(box);
+
+    } // main
+
+} // class
--- CS/lab6/rectangle.java
+++ CS/lab6/rectangle.java
@@ -3,14 +3,47 @@ import java.util.Scanner;
 class rectangle {
     public static void main(String[] args) {
         int height;
-        int width; 
+        int width;
+        String box = "";
         Scanner in = new Scanner(System.in);
 
         System.out.print("input the height and width seperated by a space:");
         height = in.nextInt();
         width = in.nextInt();
 
-        String box[][] = new String[width][height];
+        int w = width;
+        int h = height;
+        while (w > 0) {
+            box += "*";
+            w--;
+        } // top
+        System.out.println(box);
 
-    }
-}
+        box = "*";
+
+        while (h > 2) {
+
+            w = width;
+
+            while (w > 2) {
+                box += " ";
+                w--;
+            } // middle
+            box += "*";
+            System.out.println(box);
+            box = "*";
+            h--;
+        } // height
+
+        box = "";
+        w = width;
+
+        while (w > 0) {
+            box += "*";
+            w--;
+        } // bottom
+        System.out.println(box);
+
+    } // main
+
+} // class
