7a32c9f444c941f89d0952858dbe4ebe39f82ba9
BreadJuice1 <43072844+BreadJuice1@users.noreply.github.com>
2019-12-11 13:37:43 -0800

    added gamerstuf

--- CS/lab6/factorials.java
+++ CS/lab6/factorials.java
@@ -3,17 +3,16 @@ import java.util.Scanner;
 class factorials {
     public static void main(String[] args) {
         String answer = "";
-        int sum = 0;
+        int sum = 1;
         int number;
         Scanner in = new Scanner(System.in);
 
         System.out.print("input a number:");
         number = in.nextInt();
-        answer = number + "! = ";
 
-        int counter = 1;
+        answer = "1! = ";
         for (int i = 1; i <= number; i++) {
-            
+            answer += sum * i;
         }
         
         System.out.print(answer);
