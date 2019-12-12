c578d408073bf10513af02e0ab097e4c59a5f865
BreadJuice1 <43072844+BreadJuice1@users.noreply.github.com>
2019-12-12 10:26:01 -0800

    diuep

--- gamer stuff/java/joke.java
+++ gamer stuff/java/joke.java
@@ -1,25 +1,20 @@
 import java.io.File;
 import java.util.Random;
 import java.util.Scanner;
+import java.io.FileNotFoundException;
+
  class FileReading {
  
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         java.util.Random rand = new Random();
         int counter = 0;
         String punchline;
+        int number = rand.nextInt(370103);
         Scanner in = new Scanner(new File("words_alpha.txt"));
-            while (in.hasNextLine()) {
-                while (counter < rand.nextInt(466000)) {
-                    in.nextLine();
-                    counter++;
-                }
-                System.out.print(in.nextLine());
-                counter = 0;
-                while (counter < rand.nextInt(466000)) {
-                    in.nextLine();
-                    counter++;
-                }
-                System.out.print(in.nextLine());
-            } // while
+            while (counter < number) {
+                in.nextLine();
+                counter++;
+            }
+            System.out.print(in.nextLine() + " ");
          } // main
     } // class
