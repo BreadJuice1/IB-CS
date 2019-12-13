44ac68d2b70a135473447f5e6a8c2b6e20c98ce9
BreadJuice1 <43072844+BreadJuice1@users.noreply.github.com>
2019-12-12 20:42:08 -0800

    got random words

--- gamer stuff/java/joke.java
+++ gamer stuff/java/joke.java
@@ -3,17 +3,34 @@ import java.util.Random;
 import java.util.Scanner;
 import java.io.FileNotFoundException;
 
- class FileReading {
- 
+class joke {
     public static void main(String[] args) throws FileNotFoundException {
+        // initialisation
         java.util.Random rand = new Random();
-        int counter = 0;
-        String punchline[] = new String[370103];
         int number = rand.nextInt(370103);
+        int counter = 0;
+        String puchline[] = new String[2];
         Scanner in = new Scanner(new File("words_alpha.txt"));
-            while (in.hasNextLine()) {
-                
-            }
-            System.out.print(in.nextLine() + " ");
-         } // main
-    } // class
+        Scanner in2 = new Scanner(new File("words_alpha.txt"));
+        // get the words
+        while (counter < number && in.hasNextLine()) {
+            if (counter == number - 1) {
+                puchline[0] = in.nextLine();
+            } else { // if
+                in.nextLine();
+            } // else
+            counter++;
+        } // while
+        counter = 0;
+        number = rand.nextInt(370103);
+        while (counter < number && in2.hasNextLine()) {
+            if (counter == number - 1) {
+                puchline[1] = in2.nextLine();
+            } else { // if
+                in2.nextLine();
+            } // else
+        } // while
+        System.out.print(puchline[0] + " ");
+        System.out.print(puchline[1]);
+    } // main
+} // class
