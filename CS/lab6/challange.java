4776fb62161672d79aa0800671bcb69aa815c834
BreadJuice1 <43072844+BreadJuice1@users.noreply.github.com>
2019-12-17 13:07:55 -0800

    Update challange.java

--- CS/lab6/challange.java
+++ CS/lab6/challange.java
@@ -6,14 +6,22 @@ class challange {
         // init
         Scanner in = new Scanner(System.in);
         int answer;
-        int prime;
+        int number[] = new int[2];
+        Boolean found = false;
 
         // user input
         System.out.println("which prime do you want to find?");
-        prime = in.nextInt();
+        number[0] = in.nextInt();
 
         // calculate prime
-        
+        int counter = 1;
+        while (found = false) {
+            if (number[0] % counter == 0 && counter != number[0] && counter != 1) {
+                found = true;
+                number[1] = 1;
+            } else (if )
+            counter++; 
+        }
         
 
     } // challange method
