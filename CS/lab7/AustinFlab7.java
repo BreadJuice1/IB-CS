999785d64acde9238ffae813c5ddcd1417029be0
BreadJuice1 <43072844+BreadJuice1@users.noreply.github.com>
2020-01-08 13:51:44 -0800

    tested some stuff and started lab7

--- CS/lab7/AustinFlab7.java
+++ CS/lab7/AustinFlab7.java
@@ -1,1 +1,12 @@
+import java.io.Scanner;
 
+class lab7 {
+    public static void main(String[] args) {
+
+    } // main
+
+    public static fillprimearray(long [] primearray) {
+
+    } // fillprimearray
+
+} // Primes
---
+++ gamer stuff/java/buttt.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+class gamer {
+    public static void main(String[] args) {
+        Scanner pp = new Scanner(System.in);
+        System.out.print(pp.next());
+    }
+}
