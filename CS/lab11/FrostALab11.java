463f6476155e794151850793a70360c2c7e20a42
BreadJuice1 <43072844+BreadJuice1@users.noreply.github.com>
2020-03-10 13:35:44 -0700

    euyi

--- CS/lab11/FrostALab11.java
+++ CS/lab11/FrostALab11.java
@@ -1,56 +1,49 @@
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.scene.layout.Pane;
+import javafx.scene.transform.Scale;
 import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.shape.*;
+import javafx.scene.Node;
+import javafx.scene.paint.Paint;
+import javafx.scene.text.Text;
 
 public class FrostALab11 extends Application {
     public static void main(String[] args) {
-        Application.launch(args);
+        Application.launch(args);    
     }
 
     @Override
-    public void start(Stage primaryStage) {
-        Button java = new Button();
-        Button toggle = new Button();
-        primaryStage.setTitle("hello world");
-        java.setText("hello");
+    public void start (Stage primaryStage) {
+        Scale scale;
+        Line line = new Line(10, 10, 200, 10);
+        Button scaleBtn = new Button();
+        Button colourBtn = new Button();
+        primaryStage.setTitle("FrostALab11");
+        scaleBtn.setText("scale");
+        colourBtn.setText("colour");
+    
+    scaleBtn.setLayoutX(150);
+    scaleBtn.setLayoutY(350);
 
-    java.setLayoutX(60);
-    java.setLayoutY(60);
-
-    toggle.setText("on");
-    toggle.setOnAction(new EventHandler<ActionEvent>() {
-
-        @Override
-        public void handle(ActionEvent event) {
-            if (toggle.getText().equals("on")) {
-                toggle.setText("off");
-            } else if (toggle.getText().equals("off")) {
-                toggle.setText("on");
-            }
-        }
-    });
-
-    java.setOnAction(new EventHandler<ActionEvent>() {
-
-        @Override
-        public void handle(ActionEvent event) {
-            if (toggle.getText().equals("on")) {
-            System.out.println("hello world");
-            } 
-        }
-    });
-    toggle.setLayoutX(60);
-    toggle.setLayoutY(120);
+    colourBtn.setLayoutX(250);
+    colourBtn.setLayoutY(350);
+    
+    line.setStroke(Color.BLACK);
+    line.setStrokeWidth(10);        
+    line.setStrokeLineCap(StrokeLineCap.BUTT);
 
     Pane root = new Pane();
-    root.getChildren().add(java);
-    root.getChildren().add(toggle);
+    root.getChildren().add(line);
+    root.getChildren().add(colourBtn);
+    root.getChildren().add(scaleBtn);
 
-    primaryStage.setScene(new Scene(root, 300, 300));
+    primaryStage.setScene(new Scene(root, 400, 400));
     primaryStage.show();
     }
 }
---
+++ CS/lab11/lab11.bat
@@ -0,0 +1,5 @@
+@echo off
+cd C:\Users\afrost0118\Documents\GitHub\IB-CS\CS\lab11
+javac FrostALab11.java 
+java FrostALab11
+pause
