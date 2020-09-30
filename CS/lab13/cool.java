22054568c004796063ce85e1f6c67904f2bd70ef
BreaJuice1 <frostaustin459@gmail.com>
2020-09-30 16:49:06 -0700

    lab13

---
+++ CS/lab13/cool.java
@@ -0,0 +1,51 @@
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.Button;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.layout.Pane;
+import javafx.scene.transform.Scale;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.shape.*;
+import javafx.scene.Node;
+import javafx.scene.paint.Paint;
+import javafx.scene.text.Text;
+import java.util.Scanner;
+import java.io.*;
+import java.util.Random;
+
+public class cool {
+
+    public static void main(String args[]) throws IOException {
+    
+    int life[][] = new int[150][150];
+
+    System.out.println("number");
+    
+    Scanner in = new Scanner(System.in);
+    int amount = in.nextInt();
+
+    Random rm = new Random();
+
+        for (int i = 0; i <= amount; i++) {
+            int x = rm.nextInt(150);
+            int y = rm.nextInt(150);
+            
+                if (life[x][y] == 0) {
+                        life[x][y] = 1;
+
+                    } else {
+                        amount += 1;
+                    }
+            System.out.println(amount);
+        } // fill array
+
+        for (int j = 22500; j >= 0; j--) {
+            
+        }
+
+    
+    } // ur mum
+} // end
